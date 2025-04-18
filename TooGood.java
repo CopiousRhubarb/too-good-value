00a257064d5d1bceaf57c08c29fbfacf19588bc6
Ryan Moore <rmoore1618@gmail.com>
2025-04-17 21:29:04 -0700

    Rectify branch to include intro again

--- TooGood.java
+++ TooGood.java
@@ -2,6 +2,13 @@ public class TooGood
 {
     public static void main(String[] args)
     {
+        //Caitlin - Intro content
+        System.out.println("Too Good to Go");
+        System.out.println("Congradulations! You bought 1 surprise bag from Blackmarket Bakery in Oceanside.");
+        System.out.println("Your order was $5.99.");
+        System.out.println("Let's figure out how much money you saved.");
+        System.out.println("Below is a list of items on the menu. Please select the item number for each item in your surpise bag.");
+
         //Caitlin - an array of the baked goods you might get in a surprise bag and  an array of their prices
         String[] bakedGoods = {
                 "Croissant",
