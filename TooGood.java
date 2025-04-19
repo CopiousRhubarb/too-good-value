dabefbcfdf35c4c389bbf38470178c64e47f936d
Ryan Moore <rmoore1618@gmail.com>
2025-04-18 17:28:26 -0700

    Refactor into methods and menu box

--- TooGood.java
+++ TooGood.java
@@ -32,24 +32,17 @@ public class TooGood
         int choice;
         double totalValue = 0.0;
         final double COST_OF_TGTG = 5.99;
-        double amount_saved;
+        double amountSaved;
 
         //Caitlin - Intro content
-        System.out.println("Too Good to Go");
-        System.out.println("Congratulations! You bought 1 surprise bag from Blackmarket Bakery in Oceanside.");
-        System.out.printf("Your order was $%.2f.%n", COST_OF_TGTG);
-        System.out.println("Let's figure out how much money you saved.");
-        System.out.println("Below is a list of items on the menu. Please select the item number for each item in your surpise bag.");
-        System.out.println();
+        printIntro(COST_OF_TGTG);
 
         // INPUT + CALCULATION
         // Ryan - User input and calculation loop
         do
         {
-            for (int i = 0; i < bakedGoods.length; i++)
-            {
-                System.out.println(i + ". " + bakedGoods[i]);
-            }
+            printMenu(bakedGoods);
+
             choice = UtilityBelt.readInt(
                     "Choose the number corresponding to the baked good you received, or -1 if done: ",
                     -1, 9);
@@ -59,11 +52,41 @@ public class TooGood
             }
         } while (choice != -1);
 
-        amount_saved = totalValue - COST_OF_TGTG;
+        amountSaved = totalValue - COST_OF_TGTG;
 
         // OUTPUT
+        printSavings(COST_OF_TGTG, totalValue, amountSaved);
+    }
+
+    private static void printSavings(double COST_OF_TGTG, double totalValue, double amountSaved)
+    {
+        System.out.println();
         System.out.printf("Too Good To Go costs $%.2f%n", COST_OF_TGTG);
         System.out.printf("You received baked goods with a total value of $%.2f%n", totalValue);
-        System.out.printf("You saved $%.2f while preventing unnecessary food waste!", amount_saved);
+        System.out.printf("You saved $%.2f while preventing unnecessary food waste!", amountSaved);
     }
+
+    private static void printIntro(double COST_OF_TGTG)
+    {
+        System.out.println("Too Good to Go");
+        System.out.println("Congratulations! You bought 1 surprise bag from Blackmarket Bakery in Oceanside.");
+        System.out.printf("Your order was $%.2f.%n", COST_OF_TGTG);
+        System.out.println("Let's figure out how much money you saved.");
+        System.out.println("Below is a list of items on the menu. Please select the item number for each item in your surpise bag.");
+        System.out.println();
+    }
+
+    public static void printMenu(String[] bakedGoods)
+    {
+        String topBorder = String.format("╔%36s╗", " ").replace(" ", "═");
+        String bottomBorder = String.format("╚%36s╝", " ").replace(" ", "═");
+
+        System.out.println(topBorder);
+        for (int i = 0; i < bakedGoods.length; i++)
+        {
+            System.out.printf("║%-36s║%n", " " + i + ". " + bakedGoods[i]);
+        }
+        System.out.println(bottomBorder);
+    }
+
 }
