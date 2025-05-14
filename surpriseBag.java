7088016184d0674d3020fa17ed90b91f9678d86c
cjlim1092 <32859465+cjlim1092@users.noreply.github.com>
2025-05-14 14:01:02 -0700

    Create surpriseBag.java

---
+++ surpriseBag.java
@@ -0,0 +1,26 @@
+Bound Coffee Company
+  $15 value, $4.99 surprise bag
+  2110 S Coash Hwy suite c, Oceanside, Ca, 92054
+  About - (empty)
+  2 years
+  500+ meals saved
+  What you could get: bread & passtries
+  Overall experience based on 85 recent reviews 4.8 stars, pickup experience 5.0, food quality 4.7, variety of contents 4.3, food quantitiy 4.8
+
+Rose Cafe
+    $18 value, 5.99 surprise bag
+  1902 s coast hwy, oceanside, ca, 92054
+  About - welcome to rose care, a cozy cafe and lounge in Oceanside, Ca. Our cozy spot is a hidden gem fueled by our passion for serving delightful twists on classif favortites. From the moment you step through our doors , you'll feel the warmth and care that goes into every aspect of oue cafe. 
+  6 months
+  500+ meals saved
+  what you could get: bread & pastries
+  overall experience based on 218 recent reviews 4.8 stars, pickup experience 4.9, food quality 4.9, variety of contents 4.8 food quantity 4.8 
+
+  Blackmarket Bakery Oceanside
+  $18 value, $5.99 surprise bag
+  510 vista way, oceanside, ca, 92054
+  About - empty
+  1 year
+  500+ meals saved
+  what you could get: bread & pastries
+    overall experience based on 218 recent reviews 4.9 stars, pickup experience 5.0, food quality 5.0, variety of contents 4.9 food quantity 4.9
